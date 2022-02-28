35af8ac30f04ad91086b6edb7fba458ffbd25223
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-02-28 04:06:04 +0000

    update

--- finetune_sup.py
+++ finetune_sup.py
@@ -94,7 +94,7 @@ def main(args):
     repr_layers = [(i + model.num_layers + 1) % (model.num_layers + 1) for i in args.repr_layers]
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-6)
-    for epoch in range(1):
+    for epoch in range(10):
         model.train()
         for batch_idx, (labels, strs, toks) in enumerate(train_data_loader):
             print(
