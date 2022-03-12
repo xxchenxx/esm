ac14738ab17f995ca71d6cedccfc7b4ca15a7d6e
ZhenyuZhangUSTC <zzy19969@mail.ustc.edu.cn>
2022-03-12 18:18:04 +0000

    update

--- esm/data.py
+++ esm/data.py
@@ -263,6 +263,57 @@ class FireprotDBBatchedDataset(object):
         return batches
 
 
+class DirBatchedDataset(object):
+    def __init__(self, sequence_labels, sequence_strs):
+        self.sequence_labels = list(sequence_labels)
+        self.sequence_strs = list(sequence_strs)
+
+    @classmethod
+    def from_file(cls, dir, train=False, fasta_root=None):
+        from glob import glob
+        names = glob(os.path.join(dir, '*.fasta'))
+        seqs = []
+        file_names = []
+        for fasta_file_path in names:
+            with open(fasta_file_path, 'r') as f:
+                fasta = f.readlines()[1:]
+                fasta = ''.join(fasta).replace("\n", "")
+            file_names.append(fasta_file_path.split("/")[-1].split(".")[0])
+            seqs.append(fasta)
+            
+        return cls(file_names, seqs)
+
+    def __len__(self):
+        return len(self.sequence_labels)
+
+    def __getitem__(self, idx):
+        return self.sequence_labels[idx], self.sequence_strs[idx]
+
+    def get_batch_indices(self, toks_per_batch, extra_toks_per_seq=0):
+        sizes = [(len(s), i) for i, s in enumerate(self.sequence_strs)]
+        sizes.sort()
+        batches = []
+        buf = []
+        max_len = 0
+
+        def _flush_current_buf():
+            nonlocal max_len, buf
+            if len(buf) == 0:
+                return
+            batches.append(buf)
+            buf = []
+            max_len = 0
+
+        for sz, i in sizes:
+            sz += extra_toks_per_seq
+            if max(sz, max_len) * (len(buf) + 1) > toks_per_batch:
+                _flush_current_buf()
+            max_len = max(max_len, sz)
+            buf.append(i)
+
+        _flush_current_buf()
+        return batches
+
 class FireprotDirBatchedDataset(object):
     def __init__(self, sequence_labels, sequence_strs):
         self.sequence_labels = list(sequence_labels)
