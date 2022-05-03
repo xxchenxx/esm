995bc758f96cde528dcc95d200382899e22c7602
ZhenyuZhangUSTC <zzy19969@mail.ustc.edu.cn>
2022-05-03 23:34:29 +0000

    update

--- scripts/cluster.sh
+++ scripts/cluster.sh
@@ -26,7 +26,7 @@ CUDA_VISIBLE_DEVICES=3 nohup python -u finetune_sup_head_regression.py esm1b_t33
 CUDA_VISIBLE_DEVICES=0 nohup python -u finetune_sup_head_regression.py esm1b_t33_650M_UR50S data/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 2 --idx d2 --lr 2e-2 --split_file d2_8.pkl > d2_8.out &
 CUDA_VISIBLE_DEVICES=1 nohup python -u finetune_sup_head_regression.py esm1b_t33_650M_UR50S data/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 2 --idx d2 --lr 2e-2 --split_file d2_9.pkl > d2_9.out &
 
-CUDA_VISIBLE_DEVICES=1 nohup python -u finetune_sup_head_regression.py esm1b_t33_650M_UR50S data/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 2 --idx d2 --lr 2e-2 --split_file d2_9.pkl > d2_9.out &
+nohup python -u finetune_sup_head_regression_dsee_parallel.py esm1b_t33_650M_UR50S data/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 2 --idx d2 --lr 2e-2 --split_file d2_9.pkl > d2_9_S.out &
 
 nohup python -u finetune_sup_head_regression.py esm1b_t33_650M_UR50S data/d1_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 2 --idx d1 --lr 2e-2 --split_file d1_0.pkl &
 
