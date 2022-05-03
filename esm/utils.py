40a31d35918c08c9125465d6916eff8e21cd878d
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-05-03 02:23:47 +0000

    update

--- esm/utils.py
+++ esm/utils.py
@@ -19,6 +19,7 @@ def PGD_classification(x, model=None, labels=None, steps=1, gamma=0.1, eps=(1/25
     
     # Compute loss
     x_adv = x.clone()
+    x_adv.requires_grad = True
     if randinit:
         # adv noise (-eps, eps)
         x_adv += (2.0 * torch.rand(x_adv.shape).cuda() - 1.0) * eps
@@ -43,6 +44,7 @@ def PGD_classification_amino(x, model=None, labels=None, steps=1, gamma=0.1, eps
     
     # Compute loss
     x_adv = x.clone()
+    x_adv.requires_grad = True
     if randinit:
         # adv noise (-eps, eps)
         x_adv += (2.0 * torch.rand(x_adv.shape).cuda() - 1.0) * eps
@@ -69,6 +71,7 @@ def PGD_regression(x, model=None, labels=None, steps=1, gamma=0.1, eps=(1/255), 
     
     # Compute loss
     x_adv = x.clone()
+    x_adv.requires_grad = True
     if randinit:
         # adv noise (-eps, eps)
         x_adv += (2.0 * torch.rand(x_adv.shape).cuda() - 1.0) * eps
@@ -79,6 +82,7 @@ def PGD_regression(x, model=None, labels=None, steps=1, gamma=0.1, eps=(1/255), 
 
         out = model(x_adv)
         loss_adv0 = torch.nn.functional.mse_loss(out.view(out.shape[0], 1), labels)
+        print(loss_adv0)
         # loss_adv0 = torch.mean(out)
         grad0 = torch.autograd.grad(loss_adv0, x_adv, only_inputs=True)[0]
         x_adv.data.add_(gamma * torch.sign(grad0.data))
@@ -93,6 +97,7 @@ def PGD_regression_amino(x, model=None, labels=None, steps=1, gamma=0.1, eps=(1/
     
     # Compute loss
     x_adv = x.clone()
+    x_adv.requires_grad = True
     if randinit:
         # adv noise (-eps, eps)
         x_adv += (2.0 * torch.rand(x_adv.shape).cuda() - 1.0) * eps
