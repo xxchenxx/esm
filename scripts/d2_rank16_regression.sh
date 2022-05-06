46d3b49c624786eed3a56c580f3c94e2d3710d88
xxchenxx <hfercxx@hotmail.com>
2022-05-06 05:41:59 +0000

    update

---
+++ scripts/d2_rank16_regression.sh
@@ -0,0 +1,17 @@
+CUDA_VISIBLE_DEVICES=2 nohup python -u finetune_sup_head_regression_dsee.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx debug --lr 1e-2 --split_file d2/d2_1.pkl --rank 16 --lr-factor 10 > d2_r16_regression_1.out 
+
+CUDA_VISIBLE_DEVICES=2 nohup python -u finetune_sup_head_regression_dsee.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx debug --lr 1e-2 --split_file d2/d2_2.pkl --rank 16 --lr-factor 10 > d2_r16_regression_2.out 
+
+CUDA_VISIBLE_DEVICES=2 nohup python -u finetune_sup_head_regression_dsee.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx debug --lr 1e-2 --split_file d2/d2_3.pkl --rank 16 --lr-factor 10 > d2_r16_regression_3.out 
+
+CUDA_VISIBLE_DEVICES=2 nohup python -u finetune_sup_head_regression_dsee.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx debug --lr 1e-2 --split_file d2/d2_4.pkl --rank 16 --lr-factor 10 > d2_r16_regression_4.out 
+
+CUDA_VISIBLE_DEVICES=2 nohup python -u finetune_sup_head_regression_dsee.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx debug --lr 1e-2 --split_file d2/d2_5.pkl --rank 16 --lr-factor 10 > d2_r16_regression_5.out 
+
+CUDA_VISIBLE_DEVICES=2 nohup python -u finetune_sup_head_regression_dsee.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx debug --lr 1e-2 --split_file d2/d2_6.pkl --rank 16 --lr-factor 10 > d2_r16_regression_6.out 
+
+CUDA_VISIBLE_DEVICES=2 nohup python -u finetune_sup_head_regression_dsee.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx debug --lr 1e-2 --split_file d2/d2_7.pkl --rank 16 --lr-factor 10 > d2_r16_regression_7.out 
+
+CUDA_VISIBLE_DEVICES=2 nohup python -u finetune_sup_head_regression_dsee.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx debug --lr 1e-2 --split_file d2/d2_8.pkl --rank 16 --lr-factor 10 > d2_r16_regression_8.out 
+
+CUDA_VISIBLE_DEVICES=2 nohup python -u finetune_sup_head_regression_dsee.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx debug --lr 1e-2 --split_file d2/d2_9.pkl --rank 16 --lr-factor 10 > d2_r16_regression_9.out
