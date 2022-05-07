a1725d593b79454713a9a62d390e8e912b848ad9
xxchenxx <hfercxx@hotmail.com>
2022-05-07 06:17:27 +0000

    update

--- scripts/vm/0506_Q2_1.sh
+++ scripts/vm/0506_Q2_1.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 nohup python -u finetune_sup_head.py esm1b_t33_650M_UR50S ~/clean_datasets/S/S_target sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx S --lr 2e-2 --split_file ~/clean_datasets/S/S_target_classification.pkl --seed 3 --mix --wandb-name S_mix > 0506_S_mix_seed3_GPU0.out &
+# CUDA_VISIBLE_DEVICES=0 nohup python -u finetune_sup_head.py esm1b_t33_650M_UR50S ~/clean_datasets/S/S_target sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx S --lr 2e-2 --split_file ~/clean_datasets/S/S_target_classification.pkl --seed 3 --mix --wandb-name S_mix > 0506_S_mix_seed3_GPU0.out &
 
 CUDA_VISIBLE_DEVICES=1 nohup python -u finetune_sup_head.py esm1b_t33_650M_UR50S ~/clean_datasets/S/S_target sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx S --lr 2e-2 --split_file ~/clean_datasets/S/S_target_classification.pkl --seed 1 --adv --gamma 1e-5 --wandb-name S_adv > 0506_S_adv_seed1_GPU1.out &
 
