f8147d223b019202546e41d785c3ce4064ef7c0f
xxchenxx <hfercxx@hotmail.com>
2022-05-08 17:25:23 +0000

    update

---
+++ scripts/vm/0508_VM7_1.sh
@@ -0,0 +1,7 @@
+CUDA_VISIBLE_DEVICES=0 nohup python -u finetune_sup_head_gression_dsee.py esm1b_t33_650M_UR50S data/S_target sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx S --lr 1e-2 --rank 8 --lr-factor 10 --split_file S_target.pkl --seed 1 --wandb-name Sr_ds_r8_s64_adv --adv --gamma 1e-5  > 0508_Sr_r8_s64_adv_seed1_GPU0.out &
+
+CUDA_VISIBLE_DEVICES=1 nohup python -u finetune_sup_head_gression_dsee.py esm1b_t33_650M_UR50S data/S_target sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx S --lr 1e-2 --rank 8 --lr-factor 10 --split_file S_target.pkl --seed 2 --wandb-name Sr_ds_r8_s64_adv --adv --gamma 1e-5  > 0508_Sr_r8_s64_adv_seed2_GPU1.out &
+
+CUDA_VISIBLE_DEVICES=2 nohup python -u finetune_sup_head_gression_dsee.py esm1b_t33_650M_UR50S data/S_target sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx S --lr 1e-2 --rank 8 --lr-factor 10 --split_file S_target.pkl --seed 3 --wandb-name Sr_ds_r8_s64_adv --adv --gamma 1e-5  > 0508_Sr_r8_s64_adv_seed3_GPU2.out &
+
+CUDA_VISIBLE_DEVICES=3 nohup python -u finetune_sup_head_dsee.py esm1b_t33_650M_UR50S data/S_target sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx S --lr 1e-2 --rank 8 --lr-factor 10 --split_file S_target_classification.pkl --seed 1 --wandb-name S_ds_r8_s64_adv --adv --gamma 1e-5  > 0508_S_r8_s64_adv_seed1_GPU3.out &
