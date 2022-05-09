7a1772120dd743d23da99bcceaafc396122ef306
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-05-09 07:30:59 +0000

    update

---
+++ scripts/vm/0509_VM7_1.sh
@@ -0,0 +1,7 @@
+CUDA_VISIBLE_DEVICES=4 nohup python -u finetune_sup_head_sap.py esm1b_t33_650M_UR50S ~/clean_datasets/d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx d2 --lr 2e-2  --split_file ~/clean_datasets/d2/d2_8_classification.pkl --seed 1 --wandb-name d2_8_c_sap_GPU4 > 0509_d2_8_c_sap_GPU4.out &
+
+CUDA_VISIBLE_DEVICES=5 nohup python -u finetune_sup_head_sap.py esm1b_t33_650M_UR50S ~/clean_datasets/d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx d2 --lr 2e-2  --split_file ~/clean_datasets/d2/d2_9_classification.pkl --seed 1 --wandb-name d2_9_c_sap_GPU5 > 0509_d2_9_c_sap_GPU5.out &
+
+CUDA_VISIBLE_DEVICES=6 nohup python -u finetune_sup_head_sap.py esm1b_t33_650M_UR50S ~/clean_datasets/d1/d1_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 2 --idx d1 --lr 2e-2  --split_file ~/clean_datasets/d1/d1_8_classification.pkl --seed 1 --wandb-name d1_8_c_sap_GPU6 > 0509_d1_8_c_sap_GPU6.out &
+
+CUDA_VISIBLE_DEVICES=7 nohup python -u finetune_sup_head_sap.py esm1b_t33_650M_UR50S ~/clean_datasets/d1/d1_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 2 --idx d1 --lr 2e-2  --split_file ~/clean_datasets/d1/d1_9_classification.pkl --seed 1 --wandb-name d1_9_c_sap_GPU7 > 0509_d1_9_c_sap_GPU7.out &
