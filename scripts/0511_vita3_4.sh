342f0ab6d8951bc953acf033d0e7ea8615bcb334
ZhenyuZhangUSTC <zzy19969@mail.ustc.edu.cn>
2022-05-12 04:07:54 +0000

    update

--- scripts/0511_vita3_4.sh
+++ scripts/0511_vita3_4.sh
@@ -1,3 +1,3 @@
-for i in $(seq 0 9); do
+for i in $(seq 0 3); do
 CUDA_VISIBLE_DEVICES=3 nohup python -u finetune_sup_head_regression_dsee_sap.py esm1b_t33_650M_UR50S d1/d1_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 2 --idx d1 --lr 1e-3 --rank 16 --lr-factor 10 --split_file d1/d1_${i}_classification.pkl --seed 1 --wandb-name 0510_d1_${i}_adv_1e-6_seed1_GPU5_sap --adv --gamma 1e-6  > 0510_d1_r_${i}_adv_1e-6_seed1_GPU6_sap.out
 done
