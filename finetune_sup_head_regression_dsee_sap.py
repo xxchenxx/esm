c220ec272bbaedf313db1b0fbcd9f577742fabcf
ZhenyuZhangUSTC <zzy19969@mail.ustc.edu.cn>
2022-05-12 14:57:28 +0000

    update

--- finetune_sup_head_regression_dsee_sap.py
+++ finetune_sup_head_regression_dsee_sap.py
@@ -238,7 +238,7 @@ def main(args):
     optimizer2 = torch.optim.AdamW(model.parameters(), lr=args.lr / args.lr_factor, weight_decay=5e-2)
     lr_scheduler1 = torch.optim.lr_scheduler.OneCycleLR(optimizer1, max_lr=args.lr, steps_per_epoch=1, epochs=int(20))
     lr_scheduler2 = torch.optim.lr_scheduler.OneCycleLR(optimizer2, max_lr=args.lr / args.lr_factor, steps_per_epoch=1, epochs=int(20))
-    for epoch in range(6):
+    for epoch in range(10):
         model.eval()
         for batch_idx, (labels, strs, toks) in enumerate(train_data_loader):
             step += 1
