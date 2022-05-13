4d0c58bd671464f8414fb298e56079b788017d04
ZhenyuZhangUSTC <zzy19969@mail.ustc.edu.cn>
2022-05-13 15:54:37 +0000

    update

--- finetune_sup_head.py
+++ finetune_sup_head.py
@@ -142,7 +142,7 @@ def main(args):
     optimizer = torch.optim.AdamW(linear.parameters(), lr=args.lr, weight_decay=5e-2)
     lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.lr, steps_per_epoch=1, epochs=int(20))
     step = 0
-    for epoch in range(4):
+    for epoch in range(6):
         model.eval()
         for batch_idx, (labels, strs, toks) in enumerate(train_data_loader):
             step += 1
---
+++ scripts/vm/0513_VM7_5.sh
@@ -0,0 +1,7 @@
+CUDA_VISIBLE_DEVICES=4 nohup python -u finetune_sup_head.py esm1b_t33_650M_UR50S ~/clean_datasets/S/S_target sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx S --lr 1e-3 --split_file ~/clean_datasets/S/S_target_classification.pkl --seed 1 --wandb-name S_adv_1e-7_lr1e-3 --adv --gamma 1e-7  > 0513_S_adv_1e-7_seed1_GPU4.out &
+
+CUDA_VISIBLE_DEVICES=5 nohup python -u finetune_sup_head.py esm1b_t33_650M_UR50S ~/clean_datasets/S/S_target sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx S --lr 1e-3 --split_file ~/clean_datasets/S/S_target_classification.pkl --seed 2 --wandb-name S_adv_1e-7_lr1e-3 --adv --gamma 1e-7  > 0513_S_adv_1e-7_seed2_GPU5.out &
+
+CUDA_VISIBLE_DEVICES=6 nohup python -u finetune_sup_head_regression.py esm1b_t33_650M_UR50S ~/clean_datasets/S/S_target sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx S --lr 1e-3 --split_file ~/clean_datasets/S/S_target_classification.pkl --seed 3 --wandb-name S_adv_1e-7_lr1e-3 --adv --gamma 1e-7  > 0513_S_adv_1e-7_seed3_GPU6.out &
+
+CUDA_VISIBLE_DEVICES=7 nohup python -u finetune_sup_head_regression.py esm1b_t33_650M_UR50S ~/clean_datasets/S/S_target sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx S --lr 1e-3 --split_file ~/clean_datasets/S/S_target_classification.pkl --seed 4 --wandb-name S_adv_1e-7_lr1e-3 --adv --gamma 1e-7  > 0513_S_adv_1e-7_seed4_GPU7.out &
