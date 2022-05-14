a27de9dd3fa917def550e3a0dcc80b4c3a888087
xxchenxx <hfercxx@hotmail.com>
2022-05-14 16:35:14 +0000

    update

---
+++ scripts/vm/0514_MMA3_2.sh
@@ -0,0 +1,3 @@
+for i in $(seq 0 9); do
+CUDA_VISIBLE_DEVICES=1 nohup python -u finetune_sup_head_regression_dsee.py esm1b_t33_650M_UR50S ~/clean_datasets/d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx d2 --lr 1e-2 --lr-factor 10 --split_file ~/clean_datasets/d2/d2_${i}.pkl --seed 1 --wandb-name 0514_d2_${i}_r_adv_5e-7_seed1_GPU1 --adv --gamma 5e-7 --epochs 6  > 0514_d2_${i}_r_adv_5e-7_seed1_GPU1.out
+done
---
+++ scripts/vm/0514_MMA3_6.sh
@@ -0,0 +1,3 @@
+for i in $(seq 0 9); do
+CUDA_VISIBLE_DEVICES=5 nohup python -u finetune_sup_head_dsee.py esm1b_t33_650M_UR50S ~/clean_datasets/d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx d2 --lr 1e-2 --lr-factor 10 --split_file ~/clean_datasets/d2/d2_${i}_classification.pkl --seed 1 --wandb-name 0514_d2_${i}_adv_5e-7_seed1_GPU5 --adv --gamma 5e-7 --epochs 6  > 0514_d2_${i}_adv_5e-7_seed1_GPU5.out
+done
