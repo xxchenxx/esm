2a1bc73a86a720106a6190a14d171b9ff693ebe3
xxchenxx <hfercxx@hotmail.com>
2022-05-17 02:30:39 +0000

    update

--- scripts/0516_vita1.sh
+++ scripts/0516_vita1.sh
@@ -26,3 +26,10 @@ CUDA_VISIBLE_DEVICES=3 nohup python -u finetune_sup_head_dsee.py esm1b_t33_650M_
 CUDA_VISIBLE_DEVICES=2 nohup python -u finetune_sup_head_dsee.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx d2 --lr 1e-2 --lr-factor 10 --split_file d2/d2_1_classification.pkl --seed 1 --wandb-name 0516_d2_1_mix_seed1_ep10_GPU0 --mix --epochs 10  > 0516_d2_1_mix_seed1_ep10_GPU0.out & 
 
 CUDA_VISIBLE_DEVICES=3 nohup python -u finetune_sup_head_dsee.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx d2 --lr 1e-2 --lr-factor 10 --split_file d2/d2_0_classification.pkl --seed 1 --wandb-name 0516_d2_0_mix_seed1_ep10_GPU0 --mix --epochs 10  > 0516_d2_0_mix_seed1_ep10_GPU3.out & 
+
+
+
+CUDA_VISIBLE_DEVICES=3 python -u finetune_sup_head_full.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx d2 --lr 1e-2 --lr-factor 10 --split_file d2/d2_0_classification.pkl --seed 1 --epochs 10  
+
+
+CUDA_VISIBLE_DEVICES=3 python -u finetune_sup_head_regression_full.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx d2 --lr 1e-2 --lr-factor 10 --split_file d2/d2_0.pkl --seed 1 --epochs 10  
