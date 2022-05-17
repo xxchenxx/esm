748df9c0381ad7c7b725ef1c28b2c40bf04e9601
xxchenxx <hfercxx@hotmail.com>
2022-05-17 03:32:26 +0000

    update

--- scripts/0516_vita1.sh
+++ scripts/0516_vita1.sh
@@ -42,3 +42,7 @@ done
 for i in $(seq 0 7); do
 CUDA_VISIBLE_DEVICES=2 nohup python -u finetune_sup_head_regression_full.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx d2 --lr 1e-2 --lr-factor 10 --split_file d2/d2_${i}.pkl --seed 1 --wandb-name 0516_d2_${i}_r_seed2_ep10_full_GPU${i} --epochs 10  > 0516_d2_${i}_r_seed2_ep10_full_GPU${i}.out 
 done
+
+for i in $(seq 0 7); do
+CUDA_VISIBLE_DEVICES=3 nohup python -u finetune_sup_head_full.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx d2 --lr 1e-2 --lr-factor 10 --split_file d2/d2_${i}_classification.pkl --seed 1 --wandb-name 0516_d2_${i}_seed2_ep10_full_GPU${i} --epochs 10  > 0516_d2_${i}_seed2_ep10_full_GPU${i}.out 
+done
