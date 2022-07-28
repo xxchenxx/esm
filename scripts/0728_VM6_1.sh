0941f355a6a196214ee5c3235295b36fb8795753
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-07-28 22:38:17 +0000

    update

--- scripts/0728_VM6_1.sh
+++ scripts/0728_VM6_1.sh
@@ -1,3 +1,3 @@
 for i in $(seq 0 9); do
-CUDA_VISIBLE_DEVICES=1 nohup python -u finetune_sup_head_dsee_sap.py esm1b_t33_650M_UR50S d2/d2_fasta_clean sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx d2 --lr 1e-3 --rank 4 --lr-factor 10 --split_file d2/d2_${i}_classification.pkl --seed 1 --wandb-name 0728_d2_${i}_mix_1e-6_seed1_GPU1_sap_0.2_0.5 --mix --mix-beta 0.5 > 0510_d2_${i}_mix_1e-6_seed1_GPU1_sap_0.2_0.5.out 
+CUDA_VISIBLE_DEVICES=1 nohup python -u finetune_sup_head_dsee_sap.py esm1b_t33_650M_UR50S /datadrive_c/yucheng/clean_datasets/d2 sup --include mean per_tok --toks_per_batch 2048 --num_classes 5 --idx d2 --lr 1e-3 --rank 4 --lr-factor 10 --split_file d2/d2_${i}_classification.pkl --seed 1 --wandb-name 0728_d2_${i}_mix_1e-6_seed1_GPU1_sap_0.2_0.5 --mix --mix-beta 0.5 > 0510_d2_${i}_mix_1e-6_seed1_GPU1_sap_0.2_0.5.out 
 done
