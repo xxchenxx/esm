184fa379d32e59d7e567962d4318abfee7261816
ZhenyuZhangUSTC <zzy19969@mail.ustc.edu.cn>
2022-09-25 19:59:19 +0000

    update

---
+++ if1_imp.sh
@@ -0,0 +1,22 @@
+CUDA_VISIBLE_DEVICES=0 nohup python if1_d2_tune_prune_imp.py 0 2e-2 1e-4 8 0.2 > d2_0_if1_dense_1e-4_8_prune_imp.out &
+
+CUDA_VISIBLE_DEVICES=1 nohup python if1_d2_tune_prune_imp.py 1 2e-2 1e-4 8 0.2 > d2_1_if1_dense_1e-4_8_prune_imp.out &
+
+CUDA_VISIBLE_DEVICES=2 nohup python if1_d2_tune_prune_imp.py 2 2e-2 1e-4 8 0.2 > d2_2_if1_dense_1e-4_8_prune_imp.out &
+
+CUDA_VISIBLE_DEVICES=3 nohup python if1_d2_tune_prune_imp.py 3 2e-2 1e-4 8 0.2 > d2_3_if1_dense_1e-4_8_prune_imp.out &
+
+
+CUDA_VISIBLE_DEVICES=0 nohup python if1_d2_tune_prune_imp.py 4 2e-2 1e-4 8 0.2 > d2_4_if1_dense_1e-4_8_prune_imp.out &
+
+CUDA_VISIBLE_DEVICES=1 nohup python if1_d2_tune_prune_imp.py 5 2e-2 1e-4 8 0.2 > d2_5_if1_dense_1e-4_8_prune_imp.out &
+
+CUDA_VISIBLE_DEVICES=2 nohup python if1_d2_tune_prune_imp.py 6 2e-2 1e-4 8 0.2 > d2_6_if1_dense_1e-4_8_prune_imp.out &
+
+CUDA_VISIBLE_DEVICES=3 nohup python if1_d2_tune_prune_imp.py 7 2e-2 1e-4 8 0.2 > d2_7_if1_dense_1e-4_8_prune_imp.out &
+
+CUDA_VISIBLE_DEVICES=0 nohup python if1_d2_tune_prune_imp.py 8 2e-2 1e-4 8 0.2 > d2_8_if1_dense_1e-4_8_prune_imp.out &
+
+CUDA_VISIBLE_DEVICES=1 nohup python if1_d2_tune_prune_imp.py 9 2e-2 1e-4 8 0.2 > d2_9_if1_dense_1e-4_8_prune_imp.out &
+
+
